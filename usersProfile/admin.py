82ab72ed7174aac6a24ad1005cc49c43d495bcfa
beer-inder <beerinder.85@gmail.com>
2020-05-29 02:58:59 +1200

    modified admin.py

--- usersProfile/admin.py
+++ usersProfile/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import extendeduser, child
+from .models import extendeduser, child, activity, guardian, newsletter, notification
 
 
 class ExtenduserAdmin(admin.ModelAdmin):
@@ -9,10 +9,31 @@ class ExtenduserAdmin(admin.ModelAdmin):
 class ChildAdmin(admin.ModelAdmin):
     list_display = ('child_name', 'parent')
 
+    list_select_related = (
+        'user', 'guardian_name'
+    )
+
+    search_fields = ('nationality',)
+#    readonly_fields = ["child_age",]
+
     def parent(self, obj):
         return obj.user
 
 
+class Activity(admin.ModelAdmin):
+    list_display = ('activity_name', 'activity_DateTime')
+    search_fields = ('activity_name', 'activity_DateTime',)
+    filter_horizontal = ('children_list',)
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_added')
+
+
 admin.site.register(extendeduser, ExtenduserAdmin)
 admin.site.register(child, ChildAdmin)
-admin.site.site_header =   'Administration'
+admin.site.register(activity, Activity)
+admin.site.register(guardian)
+admin.site.register(newsletter, NewsletterAdmin)
+admin.site.register(notification)
+admin.site.site_header = 'Administration'
