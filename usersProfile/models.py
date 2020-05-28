a7616165309c8e9654ed50155f01952ad0bb5b2b
beer-inder <beerinder.85@gmail.com>
2020-05-29 02:56:12 +1200

    Modified models.py

--- usersProfile/models.py
+++ usersProfile/models.py
@@ -4,9 +4,11 @@ from django.contrib.auth.models import User
 
 
 class extendeduser(models.Model):
-    phone_num = models.CharField(max_length=15)
-    location = models.CharField(max_length=20)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    phone_num = models.CharField(max_length=15)
+    image = models.ImageField(upload_to='photos/', blank=True)
+    location = models.CharField(max_length=20)
 
     class Meta:
         verbose_name_plural = "ExtendedUser"
@@ -19,11 +21,20 @@ class extendeduser(models.Model):
         return self.user.username
 
 
+class guardian(models.Model):
+    guardian_name = models.CharField(max_length=50)
+    relation_guardian = models.CharField(max_length=20)
+    phone_num = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.guardian_name
+
+
 class child(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     child_name = models.CharField(max_length=50)
     parent_name = models.CharField(max_length=50)
-    child_age = models.IntegerField()
+    child_age = models.IntegerField(null=True, blank=True)
     sex = {
         ('male', 'MALE'),
         ('female', 'FEMALE'),
@@ -32,8 +43,10 @@ class child(models.Model):
     date_of_birth = models.CharField(max_length=20)
     nationality = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
-    guardian = models.CharField(max_length=50)
-    relation_guardian = models.CharField(max_length=20)
+    guardian_name = models.ForeignKey(
+        guardian, on_delete=models.SET_NULL, null=True)
+    photo_main = models.ImageField(upload_to='photos/')
+    photo_1 = models.ImageField(upload_to='photos/', blank=True)
     enrolment_date = models.DateTimeField(default=datetime.now)
 
     class Meta:
@@ -41,3 +54,43 @@ class child(models.Model):
 
     def __str__(self):
         return self.child_name
+
+
+class activity(models.Model):
+    #    teacher_name = models.ForeignKey(
+    #        User, related_name='teacher_name', on_delete=models.SET_NULL, null=True)
+    teacher_name = models.CharField(max_length=100)
+    children_list = models.ManyToManyField(child)
+    activity_name = models.CharField(max_length=500)
+    activity_description = models.CharField(max_length=1000)
+    activity_DateTime = models.DateField()
+    activity_duration = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Activity"
+
+    def __str__(self):
+        return self.activity_name
+
+
+# Command to get list of children enrolled in an activity
+        # ch=activity.objects.filter(children_list__child_name="child2beer")
+
+
+class teacher(models.Model):
+    pass
+
+
+class newsletter(models.Model):
+    email = models.EmailField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+
+class notification(models.Model):
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
